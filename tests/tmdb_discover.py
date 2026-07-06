29c1ee0c318db9beca31a869e7d123b6bcead4a7
Abdullah Mahmood <abdullah.au@outlook.com>
2026-07-06 15:28:57 +0400

    update test files

--- tests/tmdb_discover.py
+++ tests/tmdb_discover.py
@@ -1,4 +1,3 @@
-import json
 import requests
 from cinesync.utils.net import force_ipv4
 from cinesync.paths import DATA_DIR
@@ -13,6 +12,7 @@ url = f"https://api.themoviedb.org/3/discover/{content_type}"
 
 params = {
     "include_adult": "false",
+    # "with_original_language": "en",
     "vote_count.gte": 10,
     "vote_average.gte": 6.5,
     "page": 1,
@@ -22,8 +22,10 @@ if content_type == "movie":
     params["include_video"] = "false"
     params["with_runtime.gte"] = 40
     params["sort_by"] = "primary_release_date.asc"
+    # params["primary_release_date.gte"] = "1940-01-01"
 else:
     params["sort_by"] = "first_air_date.asc"
+    # params["first_air_date.gte"] = "1940-01-01"
 
 headers = {
     "accept": "application/json",
